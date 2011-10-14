353bd33918cec3290350b93d0c3dc897bf99d6cd
Stefan Richter <stefan@sblbs.de>
2011-10-14 12:33:30 +0200

    something in App.scala

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,5 +1,32 @@
-object Main {
-	def main(args: Array[String]) {
-		println("Hello, world!")
-    }
+import com.google.bitcoin.core._
+import com.google.bitcoin.discovery.DnsDiscovery
+import com.google.bitcoin.store.BoundedOverheadBlockStore
+import java.io.File
+
+
+object BlockChainDownloader {
+
+  val params = NetworkParameters.testNet
+  val wallet = new Wallet(params)
+  val blockStore = new BoundedOverheadBlockStore(params, new File("bitcoin.blockchain"));
+  val chain  = new BlockChain(params, wallet, blockStore)
+  val peers = new PeerGroup(blockStore,params,chain)
+  val listener: DownloadListener = new DownloadListener
+
+// Load the block chain, if there is one stored locally.
+  def apply()
+  {
+    println("Reading block store from disk");
+
+    peers.addPeerDiscovery(new DnsDiscovery(params))
+    peers.start()
+
+    peers.startBlockChainDownload(listener)
+  }
+}
+
+object Main extends App
+{
+  BlockChainDownloader()
+
 }
