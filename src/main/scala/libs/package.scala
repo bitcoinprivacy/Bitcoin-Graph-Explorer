b4186c377a46de537df764fb22a5d7b9d7e6700e
JorgeMartinezPizarro <jorgemartinezpizarro@gmail.com>
2014-05-12 15:56:33 +0200

    added analyze command for sqlite3

--- src/main/scala/libs/package.scala
+++ src/main/scala/libs/package.scala
@@ -16,25 +16,19 @@ package object libs
   var stepClosure = conf.getInt("closureStep")
   var stepPopulate = conf.getInt("populateStep");
 
-  def databaseSession(f: => Unit): Unit = {
+  def databaseSession(f: => Unit): Unit =
+  {
     Database.forURL(
       url = "jdbc:sqlite:"+db_file,
       driver = "org.sqlite.JDBC"
-     // user = "root",
-      //password = "12345"
     ) withSession
     {
-      (Q.u + "PRAGMA main.page_size = 4096;  ").execute
-      (Q.u + "PRAGMA main.cache_size=10000;          ").execute
-      (Q.u + "PRAGMA main.locking_mode=EXCLUSIVE;").execute
-      (Q.u + "PRAGMA main.synchronous=NORMAL;").execute
-      (Q.u + "PRAGMA main.journal_mode=WAL;").execute
-      //(Q.u + "PRAGMA main.cache_size=5000;").execute
-     // (Q.u + "PRAGMA temp_store=OFF").execute
-   //   (Q.u + "PRAGMA page_size = 39600;").execute
-   //   (Q.u + "PRAGMA journal_mode=off;").execute
-  //    (Q.u + "PRAGMA synchronous=0;").execute
-//      (Q.u + "PRAGMA cache_size= 39600;").execute
+      (Q.u + "PRAGMA main.page_size = 4096;           ").execute
+      (Q.u + "PRAGMA main.cache_size=10000;           ").execute
+      (Q.u + "PRAGMA main.locking_mode=EXCLUSIVE;     ").execute
+      (Q.u + "PRAGMA main.synchronous=NORMAL;         ").execute
+      (Q.u + "PRAGMA main.journal_mode=WAL;           ").execute
+      (Q.u + "analyze;                                ").execute
       f
     }
 
