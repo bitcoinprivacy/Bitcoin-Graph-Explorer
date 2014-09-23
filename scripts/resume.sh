959ba6ac39b524cd8312e44fc238a10771676c8b
JorgeMartinezPizarro <jorgemartinezpizarro@gmail.com>
2014-09-23 23:18:44 +0200

    Fixed

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 echo "Obtaining block list"
-scripts/getblocklist.sh
 echo "Processing blockchain"
 sbt "run resume"
 echo "updating blockcount for website"
-cat /root/bge/blockchain/blocklist.txt  | wc -l > /root/bge/blockchain/count.txt
+cat /root/.bitcoin/blockchain/blocklist.txt  | wc -l > /root/bge/blockchain/count.txt
