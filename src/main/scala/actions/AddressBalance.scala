bc5ee00a0cb5f5e6196568c73682574f4f0989c5
stefanwouldgo <info@bitcoinprivacy.net>
2014-10-04 17:47:27 +0000

    Added execute

--- src/main/scala/actions/AddressBalance.scala
+++ src/main/scala/actions/AddressBalance.scala
@@ -20,12 +20,14 @@ object AddressBalance
     (Q.u + "insert into balances SELECT sum(value) as balance, address as address FROM movements m  WHERE spent_in_transaction_hash IS NULL  GROUP BY address;").execute
     println("Creating indexes..."); 
     (Q.u + "create index address_balance on balances(address);").execute
-    (Q.u + "drop table if exists closure_balance;")
-    (Q.u + "create table closure_balance (members integer, representant blob, balance double);")
-    (Q.u + "insert into closure_balance select ifnull(count(distinct(a.hash)), 1) as members, hex(b.address) as address, sum(b.balance) total from balances b left outer join addresses a on a.hash = b.address group by a.representant;")
-    (Q.u + "create index cb1 on closure_balance(members)")
-    (Q.u + "create index cb2 on closure_balance(representant)")
-    (Q.u + "create index cb3 on closure_balance(balance)")
+    println("Drop and create closure_balances") 
+    (Q.u + "drop table if exists closure_balance;").execute
+    (Q.u + "create table closure_balance (members integer, representant blob, balance double);").execute
+    println("Inserting elements")
+    (Q.u + "insert into closure_balance select ifnull(count(distinct(a.hash)), 1) as members, hex(b.address) as address, sum(b.balance) total from balances b left outer join addresses a on a.hash = b.address group by a.representant;").execute
+    (Q.u + "create index cb1 on closure_balance(members)").execute
+    (Q.u + "create index cb2 on closure_balance(representant)").execute
+    (Q.u + "create index cb3 on closure_balance(balance)").execute
   }
   println("     Balance table updated in %s ms" format (System.currentTimeMillis - clock))
 }
