aff3629647cf91e0e4329e776fc21459481c3abb
stefanwouldgo <info@bitcoinprivacy.net>
2014-10-11 16:17:03 +0000

    Parse errors from resume to the script file. Not longer required to put the output manually into log

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -2,6 +2,8 @@
 cd /root/bge
 cat /root/.bitcoin/blocklist.txt  | wc -l > /root/bge/blockchain/count.txt.prov
 echo "Processing blockchain"
-sbt "run resume"
-echo "updating blockcount for website"
+sbt "run resume" >> blockchain/resume.log
+grep ERROR: blockchain/resume.log >> blockchain/scripts.log
+sed -i 's/ERROR://g' blockchain/scripts.log
+sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
 mv blockchain/count.txt.prov blockchain/count.txt
