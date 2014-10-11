63d77f8a3ee5d1023d6a65c752eb110e67ea25d1
stefanwouldgo <info@bitcoinprivacy.net>
2014-10-11 16:30:13 +0000

    Update the output of the script resume

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
+date
+echo "Reading blocklist"
 cd /root/bge
 cat /root/.bitcoin/blocklist.txt  | wc -l > /root/bge/blockchain/count.txt.prov
 echo "Processing blockchain"
 sbt "run resume" >> blockchain/resume.log
+echo "Parsing errors found" 
 grep ERROR: blockchain/resume.log >> blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
 sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
 mv blockchain/count.txt.prov blockchain/count.txt
+echo "Done!"
+date
