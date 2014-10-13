799b4763c1fabcb81eb0bf8ac943814caaef53c6
stefanwouldgo <info@bitcoinprivacy.net>
2014-10-13 13:44:37 +0000

    Log resume.log save just the last run. scripts.log and run.log keep historical data.

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -3,7 +3,7 @@ date
 echo "Reading blocklist"
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 echo "Processing blockchain"
-sbt "run resume" >> blockchain/resume.log
+sbt "run resume" > blockchain/resume.log
 echo "Parsing errors found" 
 grep ERROR: blockchain/resume.log >> blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
