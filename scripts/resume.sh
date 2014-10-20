6a8b1bb97a7e5124cd9d5b3ef4e9872847bda73b
stefanwouldgo <info@bitcoinprivacy.net>
2014-10-20 11:17:35 +0000

    multiple fixes

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-date
-echo "Reading blocklist"
+for pid in $(pidof -x resume.sh); do
+    if [ $pid != $$ ]; then
+        echo "[$(date)] : resume.sh : Process is already running with PID $pid"
+        exit 1
+    fi
+done
+echo "[$(date)] Reading blocklist"
 cd /root/bge
 cat /root/.bitcoin/blocklist.txt  | wc -l > /root/bge/blockchain/count.txt.prov
 echo "Processing blockchain"
@@ -10,5 +15,5 @@ grep ERROR: blockchain/resume.log >> blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
 sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
 mv blockchain/count.txt.prov blockchain/count.txt
-echo "Done!"
-date
+echo "[$(date)]<b>Done!</b>"
+
