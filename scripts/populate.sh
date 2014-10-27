989594578e775b8fe4f4c8c32e9ffc6a2fea8b3a
JorgeMartinezPizarro <jorgemartinezpizarro@gmail.com>
2014-10-27 13:33:57 +0100

    Added JVM options on run

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -3,7 +3,7 @@ date
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 echo "0" > blockchain/count.txt
 echo "Populating database"
-sbt "run populate" > blockchain/populate.log
+scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" -DXmx=10G Explorer populate > blockchain/populate.log
 echo "Parsing errors found"
 grep ERROR: blockchain/populate.log > blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
--- scripts/resume.sh
+++ scripts/resume.sh
@@ -4,6 +4,7 @@ date
 echo "Reading blocklist"
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 echo "Processing blockchain"
+scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" -DXmx=1G Explorer resume > blockchain/resume.log
 sbt "run resume" > blockchain/resume.log
 echo "Parsing errors found" 
 grep ERROR: blockchain/resume.log >> blockchain/scripts.log
