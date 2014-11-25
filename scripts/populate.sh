b381f3ab3d053d093cf5b386607dd96e5912eec8
stefanwouldgo <info@bitcoinprivacy.net>
2014-11-25 16:43:22 +0100

    fixed wrong java options for RAM size

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -3,12 +3,11 @@ date
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 echo "0" > blockchain/count.txt
 echo "[$(date)] Populating database"
-scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" -DXmx=10G Explorer populate > blockchain/populate.log
+JAVA_OPTS="-Xmx10g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer populate > blockchain/populate.log
 echo "Parsing errors found"
 grep ERROR: blockchain/populate.log > blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
 sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
-date
-echo "[$(date)] "Done!"
+echo "[$(date)] Done!"
 mv blockchain/count.txt.prov blockchain/count.txt
 
