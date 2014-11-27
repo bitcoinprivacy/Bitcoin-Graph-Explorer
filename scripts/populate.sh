0c9c009147ab09ddcbd906d239bd6c38cc3c1eee
stefanwouldgo <stefan@sblbs.de>
2014-11-27 12:19:15 +0100

    Update populate.sh
    10G are not enough.

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -3,7 +3,7 @@ date
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 echo "0" > blockchain/count.txt
 echo "[$(date)] Populating database"
-JAVA_OPTS="-Xmx10g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer populate > blockchain/populate.log
+JAVA_OPTS="-Xmx12g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer populate > blockchain/populate.log
 echo "Parsing errors found"
 grep ERROR: blockchain/populate.log > blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
