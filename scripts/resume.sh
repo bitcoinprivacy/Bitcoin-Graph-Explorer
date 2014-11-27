c9264bdf70864fa9aba0ba46ea3f513fdd134197
stefanwouldgo <info@bitcoinprivacy.net>
2014-11-27 14:06:44 +0100

    fixed some strange things in resume script, commented out extra closure table updating

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd /root/bge
-# crontab need to change directory to the bge repository folder
- for pid in $(pidof -x resume.sh); do
+# crontab needs to change directory to the bge repository folder
+for pid in $(pidof -x resume.sh); do
     if [ $pid != $$ ]; then
         #echo "[$(date)] : resume.sh : Process is already running with PID $pid"
         exit 1
@@ -10,7 +10,6 @@ done
 echo "[$(date)] Reading blockchain"
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 JAVA_OPTS="-Xmx1g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer resume > blockchain/resume.log
-cat /root/.bitcoin/blocklist.txt  | wc -l > /root/bge/blockchain/count.txt.prov
 grep ERROR: blockchain/resume.log >> blockchain/scripts.log
 sed -i 's/ERROR://g' blockchain/scripts.log
 sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
