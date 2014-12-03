871853580263e6ca35934a5e39cd75e06ff3e2c8
stefanwouldgo <info@bitcoinprivacy.net>
2014-12-03 12:59:00 +0100

    scripts cleaned

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-date
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
 echo "0" > blockchain/count.txt
 echo "[$(date)] Populating database"
 JAVA_OPTS="-Xmx12g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer populate > blockchain/populate.log
-echo "Parsing errors found"
-grep ERROR: blockchain/populate.log > blockchain/scripts.log
-sed -i 's/ERROR://g' blockchain/scripts.log
-sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
 echo "[$(date)] Done!"
+cp blockchain/populate.log blockchain/history.log
 mv blockchain/count.txt.prov blockchain/count.txt
 
--- scripts/resume.sh
+++ scripts/resume.sh
@@ -12,12 +12,10 @@ for pid in $(pidof -x resume.sh); do
         exit 1
     fi
 done
-echo "[$(date)] Reading blockchain"
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
+blocksnummer=`expr $(cat blockchain/count.txt.prov)`
+echo "[$(date)] Reading blockchain until $blocksnummer"
 JAVA_OPTS="-Xmx1g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer resume > blockchain/resume.log
-grep ERROR: blockchain/resume.log >> blockchain/scripts.log
-sed -i 's/ERROR://g' blockchain/scripts.log
-sed -i 's/)\[/)\ \[/g' blockchain/scripts.log
 mv blockchain/count.txt.prov blockchain/count.txt
 cat blockchain/resume.log >> blockchain/history.log
 echo "[$(date)] Done!"
