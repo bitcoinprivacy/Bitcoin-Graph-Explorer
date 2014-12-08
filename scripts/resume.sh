709b326a5a8e680840a476624eae40fbd779b387
JorgeMartinezPizarro <jorgemartinezpizarro@gmail.com>
2014-12-08 11:50:51 +0100

    Dont run if blocks are the same.

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -12,9 +12,16 @@ for pid in $(pidof -x resume.sh); do
         exit 1
     fi
 done
+
 cat .bitcoin/blocklist.txt  | wc -l > blockchain/count.txt.prov
-blocksnummer=`expr $(cat blockchain/count.txt.prov)`
-echo "[$(date)] Reading blockchain until $blocksnummer"
+lastBlockNumber=`expr $(cat blockchain/count.txt)`
+newBlockNumber=`expr $(cat blockchain/count.txt.prov)`
+
+if [ "$lastBlockNumber" -eq "$newBlockNumber" ]; then
+    exit 1
+fi
+
+echo "[$(date)] Reading blockchain until $lastBlockNumber"
 JAVA_OPTS="-Xmx1g" scala -classpath "target/scala-2.11/Bitcoin Graph Explorer-assembly-2.0.jar" Explorer resume > blockchain/resume.log
 mv blockchain/count.txt.prov blockchain/count.txt
 cat blockchain/resume.log >> blockchain/history.log
