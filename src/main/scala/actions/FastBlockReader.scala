4632db280eef417e201ed8f06ed61842379003dd
Jorge and Stefan <info@bitcoinprivacy.net>
2015-04-24 12:46:24 +0000

    forgot one key length

--- src/main/scala/actions/FastBlockReader.scala
+++ src/main/scala/actions/FastBlockReader.scala
@@ -100,7 +100,7 @@ trait FastBlockReader extends BlockReader
     val time = System.currentTimeMillis
     Q.updateNA("create index  address on movements (address (20));").execute
     Q.updateNA("create unique index  transaction_hash_i on movements (transaction_hash (20), `index`);").execute
-    Q.updateNA("create index  spent_in_transaction_hash2 on movements (spent_in_transaction_hash (20), address);").execute
+    Q.updateNA("create index  spent_in_transaction_hash2 on movements (spent_in_transaction_hash (20), address(20));").execute
     Q.updateNA("create index  block_height on movements (block_height);").execute
     Q.updateNA("create index  block_hash on blocks(hash (20));").execute
     Q.updateNA("create index  block_height2 on blocks(block_height);").execute
