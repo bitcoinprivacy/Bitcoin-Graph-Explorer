f9646d9793ce62bbafd364ddcfead011affa1527
= <=>
2016-02-15 15:08:14 +0100

    fixed UTXO api

--- src/main/scala/net/bitcoinprivacy/bge/models/UTXO.scala
+++ src/main/scala/net/bitcoinprivacy/bge/models/UTXO.scala
@@ -7,40 +7,41 @@ import scala.slick.jdbc.{ StaticQuery => Q }
 import scala.slick.jdbc.meta.MTable
 import util.Hash
 
-case class UTXO(tx: String, value:Long)
+case class UTXO(tx: String, value:Long, address: String)
+case class UTXOsSummary(count : Int, sum: Long)
 
-object UTXO extends core.BitcoinDB 
+object UTXO extends core.BitcoinDB
 {
   def getUtxosByAd(address: Array[Byte], from: Int, until: Int) =
     transactionDBSession{
 
+      val ad = Address.hashToAddress(address)
       val utxos = for (b<- utxo.filter(_.address===address).drop(from).take(until-from)) 
                        yield (b.transaction_hash ,b.value)
 
-      utxos.run.toVector map (p => UTXO(Hash(p._1).toString, p._2))
+      utxos.run.toVector map (p => UTXO(Hash(p._1).toString, p._2, ad))
 
     }
 
   def getUtxosByTx(transactionHash: Array[Byte], from: Int, until: Int) =
     transactionDBSession {
 
+      val tx = Hash(transactionHash).toString
       val outputsFromUTXOS = for (b<-utxo.filter(_.transaction_hash===transactionHash).drop(from).take(until-from))
-                             yield (b.transaction_hash, b.value)
+                             yield (b.address, b.value)
 
-      outputsFromUTXOS.run.toVector map (p => UTXO(Hash(p._1).toString, p._2))
+      outputsFromUTXOS.run.toVector map (p => UTXO(tx, p._2, Address.hashToAddress(p._1)))
     }
 
   def getUtxosByAdSummary(address: Array[Byte]) =
     transactionDBSession{
-      Summary(utxo.filter(_.address===address).size.run)
+      val query = utxo.filter(_.address===address).map(_.value)
+      UTXOsSummary(query.size.run, query.sum.run.getOrElse(0L))
     }
 
   def getUtxosByTxSummary(transactionHash: Array[Byte]) =
     transactionDBSession {
-      Summary(utxo.filter(_.transaction_hash===transactionHash).size.run)
+      val query = utxo.filter(_.transaction_hash===transactionHash).map(_.value)
+      UTXOsSummary(query.size.run, query.sum.run.getOrElse(0L))
     }
-
 }
-
-
-
