78734867b946605853690f199365163dde194a26
root <root@vmd24050.contaboserver.net>
2018-05-19 15:52:49 +0200

    move test script to test folder. rename old spec to bge spec

--- src/test/scala/BGESpec.scala
+++ src/test/scala/BGESpec.scala
@@ -2,10 +2,13 @@ import sys.process._
 import util._
 import org.scalatest._
 
-class PopulateSpec extends FlatSpec with Matchers {
+class BGESpec extends FlatSpec with Matchers {
   val BITCOIN = "docker exec --user bitcoin regtestbge_bitcoin_1 bitcoin-cli -regtest -rpcuser=foo -rpcpassword=bar -rpcport=18333"
+  // fixme: to use an script is dirty but we need to access to bitcoincli which lives in a container
   def gen(i:Int) = (BITCOIN + " generate " + i.toString) ! ProcessLogger(_ => ())
-  def resetRegtest = "/root/Bitcoin-Graph-Explorer/test.sh" ! ProcessLogger(_ => ())
+  def resetRegtest = "/root/Bitcoin-Graph-Explorer/src/test/resources/test.sh" ! ProcessLogger(_ => ()) 
+  // fixme: to use an script is dirty but i need to restart some containers...
+
 
   "Populate" should "safe 5 blocks as the blockchain contains 5 blocks" in {
     resetRegtest
