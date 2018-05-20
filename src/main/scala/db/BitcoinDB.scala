77e300dd2dde4316382faabe3a134bbbbaa5bf90
root <root@vmd24050.contaboserver.net>
2018-05-20 14:21:35 +0200

    remove redundant log - debug

--- src/main/scala/db/BitcoinDB.scala
+++ src/main/scala/db/BitcoinDB.scala
@@ -27,7 +27,6 @@ trait BitcoinDB {
   def DBNAME = conf.getString("databaseName")
   def URL = "jdbc:postgresql://" + HOST + "/" + DBNAME + OPTIONS
   def DRIVER = "org.postgresql.Driver"
-  println(URL)
 
   def statsDone: Boolean = {
     DB.withSession{
--- src/test/scala/BGESpec.scala
+++ src/test/scala/BGESpec.scala
@@ -3,15 +3,17 @@ import util._
 import org.scalatest._
 
 class BGESpec extends FlatSpec with Matchers {
+  
   val BITCOIN = "docker exec --user bitcoin docker_bitcoin_1 bitcoin-cli -regtest -rpcuser=foo -rpcpassword=bar -rpcport=18333"
+  
   // fixme: to use an script is dirty but we need to access to bitcoincli which lives in a container
-  def gen(i:Int) = (BITCOIN + " generate " + i.toString) ! //ProcessLogger(_ => ())
+  
+  def gen(i:Int) = (BITCOIN + " generate " + i.toString) ! ProcessLogger(_ => ())
 
-  def startDocker = "/root/Bitcoin-Graph-Explorer/src/test/docker/start.sh" ! //ProcessLogger(_ => ()) 
+  def startDocker = "/root/Bitcoin-Graph-Explorer/src/test/docker/start.sh" ! ProcessLogger(_ => ()) 
 
-  def stopDocker = "/root/Bitcoin-Graph-Explorer/src/test/docker/stop.sh" ! //ProcessLogger(_ => ())
+  def stopDocker = "/root/Bitcoin-Graph-Explorer/src/test/docker/stop.sh" ! ProcessLogger(_ => ())
 
-  // fixme: to use an script is dirty but i need to restart some containers...
   // for some reason starting bge 2 times in same process fails... maybe cause singleton pattern?
 
   "Populate" should "save 10 blocks as the blockchain contains 10 blocks" in {
@@ -19,9 +21,12 @@ class BGESpec extends FlatSpec with Matchers {
     gen(10)
     Explorer.populate
     Explorer.blockCount should be (11)
+    // stopDocker
   }
 
   "Resume" should "after add 10 blocks, resume save the 10 blocks" in {
+    // for some reason starting bge 2 times in same process fails... maybe cause we use Objects?
+    // startDocker
     gen(10)
     Explorer.resume
     val result = Explorer.blockCount
