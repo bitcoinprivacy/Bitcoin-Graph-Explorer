10eb9ea60d25f1cd7f9b5315055f1c6346c34be4
root <root@vmd24050.contaboserver.net>
2018-05-20 09:08:04 +0200

    change folders

--- src/test/docker/reset.sh
+++ src/test/docker/reset.sh
@@ -1,16 +1,8 @@
-rm /root/Bitcoin-Graph-Explorer/src/test/data/bitcoin/regtest/ -rf
+mkdir -p /root/Bitcoin-Graph-Explorer/src/test/data/psql
+mkdir -p /root/Bitcoin-Graph-Explorer/src/test/data/bitcoin
+rm /root/Bitcoin-Graph-Explorer/src/test/data/bitcoin/ -rf
 rm /root/Bitcoin-Graph-Explorer/src/test/data/blockchain/ -rf
 cd /root/Bitcoin-Graph-Explorer/src/test/docker
 make stop start
 echo "waiting for components to be ready"
-echo "5"
-sleep 1s
-echo "4"
-sleep 1s
-echo "3"
-sleep 1s
-echo "2"
-sleep 1s
-echo "1"
-sleep 1s
-echo "done sleeping"
+sleep 5s
